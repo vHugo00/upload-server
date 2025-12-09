b7143076831ccc8529a11a72e60ae32bc6a973ea
Vitor Hugo <vv001226@gmail.com>
2025-12-09 19:28:04 -0300

    Variáveis ambiente com Zod e Docker compose

--- src/infra/http/server.ts
+++ src/infra/http/server.ts
@@ -1,10 +1,13 @@
 import { fastifyCors } from '@fastify/cors'
 import { fastify } from 'fastify'
+import { env } from '@/env'
 
 const server = fastify()
 
 server.register(fastifyCors, { origin: '*' })
 
+console.log(env.DATABASE_URL)
+
 server.listen({ port: 3333, host: '0.0.0.0' }).then(() => {
-  console.log('HTTP server running!')
+  console.log('HTTP Server running!')
 })
