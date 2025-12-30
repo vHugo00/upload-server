3ac3b8fb056fab7d31d432e929e8055d0eaa16ba
Vitor Hugo <vv001226@gmail.com>
2025-12-30 15:28:14 -0300

    Servidor HTTP com Fastify

--- src/infra/http/server.ts
+++ src/infra/http/server.ts
@@ -1,13 +1,33 @@
 import { fastifyCors } from '@fastify/cors'
 import { fastify } from 'fastify'
-import { env } from '@/env'
+import {
+  hasZodFastifySchemaValidationErrors,
+  serializerCompiler,
+  validatorCompiler,
+} from 'fastify-type-provider-zod'
+import { uploadImageRoute } from './routes/upload-image'
 
 const server = fastify()
 
 server.register(fastifyCors, { origin: '*' })
 
-console.log(env.DATABASE_URL)
+server.setValidatorCompiler(validatorCompiler)
+server.setSerializerCompiler(serializerCompiler)
+
+server.setErrorHandler((error, request, reply) => {
+  if (hasZodFastifySchemaValidationErrors(error)) {
+    return reply.status(400).send({
+      message: 'Validation error.',
+      issues: error.validation,
+    })
+  }
+
+  console.error(error)
+  return reply.status(500).send({ message: 'Internal server error.' })
+})
+
+server.register(uploadImageRoute)
 
 server.listen({ port: 3333, host: '0.0.0.0' }).then(() => {
-  console.log('HTTP Server running!')
+  console.log('HTTP Server running! ✅')
 })
