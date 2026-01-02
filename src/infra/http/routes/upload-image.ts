6f7912c7de5f0c909b13c2fea1a06cc9da34b45e
Vitor Hugo <vv001226@gmail.com>
2026-01-02 15:39:09 -0300

    Estratégias de IDs

--- src/infra/http/routes/upload-image.ts
+++ src/infra/http/routes/upload-image.ts
@@ -1,5 +1,7 @@
 import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
 import z from 'zod'
+import { db } from '@/infra/db'
+import { schema } from '@/infra/db/schemas'
 
 export const uploadImageRoute: FastifyPluginAsyncZod = async server => {
   server.post(
@@ -20,6 +22,12 @@ export const uploadImageRoute: FastifyPluginAsyncZod = async server => {
       },
     },
     async (request, reply) => {
+      await db.insert(schema.uploads).values({
+        name: 'teste.jpg',
+        remoteKey: 'teste.jpg',
+        remote_url: 'https://localhost.com',
+      })
+
       return reply.status(201).send({ uploadId: 'teste' })
     }
   )
