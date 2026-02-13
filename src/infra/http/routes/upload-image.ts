a6f327fe72431c5049ad6817592d977d37b3b8e7
Vitor Hugo <vv001226@gmail.com>
2026-02-13 17:36:57 -0300

    Validando tamanho das imagens

--- src/infra/http/routes/upload-image.ts
+++ src/infra/http/routes/upload-image.ts
@@ -33,6 +33,12 @@ export const uploadImageRoute: FastifyPluginAsyncZod = async server => {
         contentStream: uploadedFile.file,
       })
 
+      if (uploadedFile.file.truncated) {
+        return reply.status(400).send({
+          message: 'File size limit reached',
+        })
+      }
+
       if (isRight(result)) {
         console.log(unwrapEither(result))
 
