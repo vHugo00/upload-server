e6f9f114ff095250b20606cd89406469629fa6ca
Vitor Hugo <vv001226@gmail.com>
2026-06-30 19:18:10 -0300

    Configurando build da aplicação

--- src/infra/http/server.ts
+++ src/infra/http/server.ts
@@ -1,4 +1,6 @@
 import { env } from '@/env'
+import { exportUploadsRoute } from '@/infra/http/routes/export-uploads'
+import { getUploadsRoute } from '@/infra/http/routes/get-uploads'
 import { uploadImageRoute } from '@/infra/http/routes/upload-image'
 import { transformSwaggerSchema } from '@/infra/http/transform-swagger-schema'
 import { fastifyCors } from '@fastify/cors'
@@ -50,6 +52,8 @@ server.register(fastifySwaggerUi, {
 })
 
 server.register(uploadImageRoute)
+server.register(getUploadsRoute)
+server.register(exportUploadsRoute)
 
 server.listen({ port: 3333, host: '0.0.0.0' }).then(() => {
   console.log('HTTP Server running!')
