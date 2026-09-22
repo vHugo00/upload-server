3e30101b64aa5c9d47223eb0d3a8f7e1cb70d1ef
Vitor Hugo <vv001226@gmail.com>
2026-09-21 21:42:21 -0300

    new: setup cache container build test

--- src/infra/http/server.ts
+++ src/infra/http/server.ts
@@ -56,5 +56,5 @@ server.register(getUploadsRoute)
 server.register(exportUploadsRoute)
 
 server.listen({ port: 3333, host: '0.0.0.0' }).then(() => {
-  console.log('HTTP Server running!')
+  console.log('HTTP Server running!!!')
 })
